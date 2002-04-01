559d7279ff11e02224f9409ac6f7c9dabf138381
pic <pic@gmail.com>
2002-04-01 12:06:17 +0000

    Removed unneccessary normalization.
    r26 matlab/trunk

--- @quaternion/inv.m
+++ @quaternion/inv.m
@@ -1,11 +1,15 @@
-%INV	Invert a unit-quaternion
+%INV Invert a unit-quaternion
 %
 %	QI = inv(Q)
 %
-%	Return the inverse of the unit-quaternion Q.
+% Return the inverse of the unit-quaternion Q.
 %
 
-%	Copyright (C) 1993 Peter Corke
+% $Log: not supported by cvs2svn $
+% $Revision: 1.2 $
+%
+% Copyright (C) 1999-2002, by Peter I. Corke
+
 function qi = inv(q)
 
-	qi = quaternion([q.s -q.v] / norm(q)^2);
+	qi = quaternion([q.s -q.v]);
