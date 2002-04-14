70cf270fb30b443e67db35a6de0a556c00a150ff
pic <pic@gmail.com>
2002-04-14 10:15:03 +0000

    Added see also line.
    r37 matlab/trunk

--- ftrans.m
+++ ftrans.m
@@ -4,9 +4,15 @@
 %
 % Transforms a force/moment F in the base frame to FT in the frame T.
 % F and FT are 6-vectors of the form [Fx Fy Fz Mx My Mz]
+%
+% SEE ALSO: DIFF2TR
 
 % $Log: not supported by cvs2svn $
-% $Revision: 1.2 $
+% Revision 1.2  2002/04/01 11:47:13  pic
+% General cleanup of code: help comments, see also, copyright, remnant dh/dyn
+% references, clarification of functions.
+%
+% $Revision: 1.3 $
 % Copyright (C) 1999-2002, by Peter I. Corke
 
 function Ft = ftrans(T, F)
