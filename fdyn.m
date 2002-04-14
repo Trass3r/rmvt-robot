791f1f06451d2a1a08e731509f43e9ee7e034a18
pic <pic@gmail.com>
2002-04-14 10:14:04 +0000

    Added support for extra command line arguments passed to torqfun. Update comments.
    r36 matlab/trunk

--- fdyn.m
+++ fdyn.m
@@ -3,31 +3,39 @@
 %	[T Q QD] = FDYN(ROBOT, T0, T1)
 %	[T Q QD] = FDYN(ROBOT, T0, T1, TORQFUN)
 %	[T Q QD] = FDYN(ROBOT, T0, T1, TORQFUN, Q0, QD0)
+%	[T Q QD] = FDYN(ROBOT, T0, T1, TORQFUN, Q0, QD0, ARG1, ARG2, ...)
 %
 % Integrates the dynamics of manipulator ROBOT dynamics over the time 
-% interval and returns vectors of joint position and velocity.
+% interval T0 to T1 and returns vectors of joint position and velocity.
 % ROBOT is a robot object and describes the manipulator dynamics and 
 % kinematics, and Q is an n element vector of joint state.
 %
 % A control torque may be specified by a user specified function
 %
-% 	TAU = TORQFUN(T, X)
+% 	TAU = TORQFUN(T, Q, QD, ARG1, ARG2, ...)
 %
-% where X is a 2n-element column vector [Q; QD], and T is the current 
-% time.	If TORQFUN is not specified, or is given as 0,  then zero 
-% torque is applied to the manipulator joints.
+% where Q and QD are the manipulator joint coordinate and velocity state 
+% respectively], and T is the current time. Optional arguments passed to FDYN
+% will be passed through to the user function.
 %
-% See also: ACCEL, RNE, ROBOT, ODE45.
+% If TORQFUN is not specified, or is given as 0,  then zero torque is 
+% applied to the manipulator joints.
+%
+% See also: ACCEL, NOFRICTION, RNE, ROBOT, ODE45.
 
 % Copyright (C) 1993 Peter Corke
 % MOD HISTORY
 % 	4/99 add object support
 % $Log: not supported by cvs2svn $
-% $Revision: 1.3 $
+% Revision 1.3  2002/04/01 11:47:13  pic
+% General cleanup of code: help comments, see also, copyright, remnant dh/dyn
+% references, clarification of functions.
+%
+% $Revision: 1.4 $
 
 % Copyright (C) 1999 Peter Corke
 
-function [t, q, qd] = fdyn(robot, t0, t1, torqfun, q0, qd0)
+function [t, q, qd] = fdyn(robot, t0, t1, torqfun, q0, qd0, varargin)
 
 	% check the Matlab version, since ode45 syntax has changed
 	v = ver;
@@ -41,11 +49,11 @@ function [t, q, qd] = fdyn(robot, t0, t1, torqfun, q0, qd0)
 		x0 = zeros(2*n,1);
 	elseif nargin == 4,
 		x0 = zeros(2*n, 1);
-	elseif nargin == 6,
+	elseif nargin >= 6,
 		x0 = [q0(:); qd0(:)];
 	end
 		
-	[t,y] = ode45('fdyn2', [t0 t1], x0, [], robot, torqfun);
+	[t,y] = ode45('fdyn2', [t0 t1], x0, [], robot, torqfun, varargin{:});
 	q = y(:,1:n);
 	qd = y(:,n+1:2*n);
 
--- fdyn2.m
+++ fdyn2.m
@@ -18,10 +18,14 @@
 % MOD HISTORY
 % 	4/99 add object support
 % $Log: not supported by cvs2svn $
-% $Revision: 1.1 $
+% Revision 1.1  2002/04/01 11:47:12  pic
+% General cleanup of code: help comments, see also, copyright, remnant dh/dyn
+% references, clarification of functions.
+%
+% $Revision: 1.2 $
 % Copyright (C) 1999-2002, by Peter I. Corke
 
-function xd = fdyn2(t, x, flag, robot, torqfun)
+function xd = fdyn2(t, x, flag, robot, torqfun, varargin)
 
 	n = robot.n;
 
@@ -30,7 +34,7 @@ function xd = fdyn2(t, x, flag, robot, torqfun)
 
 	% evaluate the torque function if one is given
 	if isstr(torqfun)
-		tau = feval(torqfun, t, q, qd);
+		tau = feval(torqfun, t, q, qd, varargin{:});
 	else
 		tau = zeros(n,1);
 	end
