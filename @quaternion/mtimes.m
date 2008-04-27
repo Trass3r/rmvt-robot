8004c6a29ed6f8816c5f7015b9e1b0f8de0cf1c1
cor134 <cor134@gmail.com>
2008-04-27 10:19:13 +0000

    Handle quaternion * scalar case
    r80 matlab/trunk

--- @quaternion/mtimes.m
+++ @quaternion/mtimes.m
@@ -4,15 +4,19 @@
 %
 % q1*q2	standard quaternion multiplication
 % q1*v	rotate vector v by quaternion
+% q1*s	multiply vector v by scalar
 
 % $Log: not supported by cvs2svn $
-% $Revision: 1.3 $
+% Revision 1.3  2002/04/01 12:06:47  pic
+% General tidyup, help comments, copyright, see also, RCS keys.
+%
+% $Revision: 1.4 $
 %
 % Copyright (C) 1999-2002, by Peter I. Corke
 
 function qp = mtimes(q1, q2)
 
-	if isa(q2, 'quaternion')
+	if isa(q1, 'quaternion') & isa(q2, 'quaternion')
 	%QQMUL	Multiply unit-quaternion by unit-quaternion
 	%
 	%	QQ = qqmul(Q1, Q2)
@@ -30,7 +34,7 @@ function qp = mtimes(q1, q2)
 		% form the product
 		qp = quaternion([s1*s2-v1*v2' s1*v2+s2*v1+cross(v1,v2)]);
 
-	elseif all(size(q2) == [3 1])
+	elseif isa(q1, 'quaternion') & isa(q2, 'double'),
 
 	%QVMUL	Multiply vector by unit-quaternion
 	%
@@ -45,6 +49,22 @@ function qp = mtimes(q1, q2)
 	% MOD HISTORY
 	%	fixed error in q-v product, added inv(q1) on RHS
 
-		qp = q1 * quaternion([0 q2']) * inv(q1);
-		qp = qp.v;
+		if length(q2) == 3,
+			qp = q1 * quaternion([0 q2(:)']) * inv(q1);
+			qp = qp.v;
+		elseif length(q2) == 1,
+			qp = quaternion( double(q1)*q2);
+		else
+			error('quaternion-vector product: must be a 3-vector or scalar');
+		end
+
+	elseif isa(q2, 'quaternion') & isa(q1, 'double'),
+		if length(q1) == 3,
+			qp = q2 * quaternion([0 q1(:)']) * inv(q2);
+			qp = qp.v;
+		elseif length(q1) == 1,
+			qp = quaternion( double(q2)*q1);
+		else
+			error('quaternion-vector product: must be a 3-vector or scalar');
+		end
 	end
