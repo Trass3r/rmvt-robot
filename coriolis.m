435b5bf26813e41209eb6de0deca9fea0c830671
cor134 <cor134@gmail.com>
2008-04-27 11:34:58 +0000

    Handle trajectory arguments.
    r84 matlab/trunk

--- coriolis.m
+++ coriolis.m
@@ -7,10 +7,10 @@
 % ROBOT is a robot object and describes the manipulator dynamics and 
 % kinematics.
 %
-% If Q and QD are row vectors, CORIOLIS(DYN,Q,QD) is a row vector 
+% If Q and QD are row vectors, CORIOLIS(ROBOT,Q,QD) is a row vector 
 % of joint torques.
 % If Q and QD are matrices, each row is interpretted as a joint state 
-% vector, and CORIOLIS(DYN,Q,QD) is a matrix each row being the 
+% vector, and CORIOLIS(ROBOT,Q,QD) is a matrix each row being the 
 % corresponding joint %	torques.
 %
 % See also: ROBOT, RNE, ITORQUE, GRAVLOAD.
@@ -20,7 +20,24 @@
 % MOD HISTORY
 % 	4/99 add object support
 % $Log: not supported by cvs2svn $
-% $Revision: 1.2 $
+% Revision 1.2  2002/04/01 11:47:11  pic
+% General cleanup of code: help comments, see also, copyright, remnant dh/dyn
+% references, clarification of functions.
+%
+% $Revision: 1.3 $
 
 function c = coriolis(robot, q, qd)
-	c = rne(robot, q, qd, zeros(size(q)), [0;0;0]);
+
+	if nargin == 3,
+		c = rne(robot, q, qd, zeros(size(q)), [0;0;0]);
+	else
+		n = length(q);
+		c = [];
+		qd = zeros(1,n);
+		for i=1:n,
+			qd(i) = 1;
+			C = coriolis(robot, q, qd);
+			qd(i) = 0;
+			c(:,i) = C';
+		end
+	end
