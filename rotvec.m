de57782df122f57705f51c535b5618497aaa9cc1
cor134 <cor134@gmail.com>
2008-04-27 09:50:54 +0000

    Remove debug print
    r78 matlab/trunk

--- rotvec.m
+++ rotvec.m
@@ -12,7 +12,11 @@
 % CHANGES:
 % 7/01	unitize the vector
 % $Log: not supported by cvs2svn $
-% $Revision: 1.3 $
+% Revision 1.3  2002/04/01 11:47:16  pic
+% General cleanup of code: help comments, see also, copyright, remnant dh/dyn
+% references, clarification of functions.
+%
+% $Revision: 1.4 $
 
 function r = rotvec(v, t)
 
@@ -24,5 +28,4 @@ function r = rotvec(v, t)
 	r =    [ct		-v(3)*st	v(2)*st
 		v(3)*st		ct		-v(1)*st
 		-v(2)*st	v(1)*st		ct	];
-		r
 	r = [v*v'*vt+r zeros(3,1); 0 0 0 1];
