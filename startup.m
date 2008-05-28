53d45210abe35532ce90d6fa55a8bbc99c111399
cor134 <cor134@gmail.com>
2008-05-28 11:46:49 +0000

    startup file to be executed when Matlab starts.  Displays a banner and adds demos to the path.
    r105 matlab/trunk

---
+++ startup.m
@@ -0,0 +1,4 @@
+disp('Robotics Toolbox for Matlab (release 8)')
+disp('  (c) Peter Corke 1992-2008 http://www.petercorke.com')
+tbpath = fileparts(which('fkine'));
+addpath( fullfile(tbpath, 'demos') );
