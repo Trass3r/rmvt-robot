6c6288f945ee2cc03be0d7172e0d11bb80cd852d
cor134 <cor134@gmail.com>
2008-05-28 11:16:41 +0000

    Tidyup to standard layout.
    r93 matlab/trunk

--- Fanuc10L.m
+++ Fanuc10L.m
@@ -1,13 +1,20 @@
-%##########################################################
+% FANUC10L  Load kinematic data for a Fanuc AM120iB/10L robot 
+%
+%	Fanuc10L
+%
+% Defines the object 'R' in the current workspace which describes the 
+% kinematic characterstics of a Fanuc AM120iB/10L
+% using standard DH conventions.
+%
+% Also define the vector q0 which corresponds to mastering position.
+%
 %Wynand Swart
 %Mega Robots CC, P/O Box 8412, Pretoria, 0001, South Africa
 %Cell: 073-1555-430
 %wynand.swart@gmail.com
 %30 Sep 2007
 %Fanuc AM120iB/10L robot
-%##########################################################
-clear all;
-%##########################################################
+
 L1 = link([-pi/2   0.15   0         0       0  ]);
 L2 = link([ pi     0.77   0         0       0  ]);
 L3 = link([-pi/2   0.1    0         0       0  ]);
@@ -20,6 +27,4 @@ L6 = link([ 0      0      0        -0.1     0  ]);
 q0 =[0   -pi/2   0   0   0   0];
 R=robot({L1 L2 L3 L4 L5 L6});
 R.name='Fanuc AM120iB/10L';
-plot(R,q0);
-drivebot(R);
 %##########################################################
--- MotomanHP6.m
+++ MotomanHP6.m
@@ -1,12 +1,20 @@
-%##########################################################
+%MotomanHP6  Load kinematic data for a Motoman HP6 manipulator
+%
+%	MotomanHP6
+%
+% Defines the object 'R' in the current workspace which describes the 
+% kinematic characterstics of a Motoman HP6 manipulator
+% using standard DH conventions.
+%
+% Also define the vector q0 which corresponds to the zero position.
+%
 %Wynand Swart
 %Mega Robots CC, P/O Box 8412, Pretoria, 0001, South Africa
 %Cell: 073-1555-430
 %wynand.swart@gmail.com
 %30 Sep 2007
 %Motoman HP robot
-%##########################################################
-clear all;
+
 %##########################################################
 L1 = link([ -pi/2   0.15     0   0            0]);
 L2 = link([ -pi     0.57     0   0            0]);
@@ -20,6 +28,4 @@ L6 = link([  pi     0        0  -0.095        0]);
 q0 =[0   -pi/2   0   0   -pi/2   0];
 R=robot({L1 L2 L3 L4 L5 L6});
 R.name='Motoman HP6';
-plot(R,q0);
-drivebot(R);
 %##########################################################
