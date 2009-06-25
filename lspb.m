8b06451936d2efc64652ed3849afc94a619d7e27
pic <pic@gmail.com>
2009-06-25 07:19:03 +0000

    Path planning primitives
    r239 matlab/branches/book

--- lspb.m
+++ lspb.m
@@ -1,161 +1,110 @@
 %LSPB  Linear segment with parabolic blend
 %
-% [S,SD,SDD] = LSPB(S0, SF, M) is a scalar trajectory (Mx1) that varies 
-% smoothly from S0 to SF in M steps using a constant velocity segment and 
-% parabolic blends (a trapezoidal path).  Velocity and acceleration can be
-% optionally returned as SD (Mx1) and SDD (Mx1).
+% Compute a function, q(t), that varies smoothly from q0 to q1, using
+% a constant velocity segment and parabolic blends (trapezoidal path).
 %
-% [S,SD,SDD] = LSPB(S0, SF, M, V) as above but specifies the velocity of 
-% the linear segment which is normally computed automatically.
+% q = lspb(q0, q1, N)
 %
-% [S,SD,SDD] = LSPB(S0, SF, T) as above but specifies the trajectory in 
-% terms of the length of the time vector T (Mx1).
+%   Return path for N steps.
 %
-% [S,SD,SDD] = LSPB(S0, SF, T, V) as above but specifies the velocity of 
-% the linear segment which is normally computed automatically and a time
-% vector.
+% q = lspb(q0, q1, t)
 %
-% LSPB(S0, SF, M, V) as above but plots S, SD and SDD versus time in a single
-% figure.
+%   Return path over the time vector t.
 %
-% Notes::
-% - Velocity is in units of distance per trajectory step, not per second.
-% - Acceleration is in units of distance per trajectory step squared, not
-%   per second squared. 
-% - For some values of V no solution is possible and an error is flagged.
+% q = lspb(q0, q1, t, V)
 %
-% References::
-% - Robotics, Vision & Control, Chap 3,
-%   P. Corke, Springer 2011.
+%   Return path over the time vector t, but setting the velocity of the
+%   linear segment.
 %
-% See also TPOLY, JTRAJ.
-
-
-% Copyright (C) 1993-2015, by Peter I. Corke
+% t = lspb(q0, q1, [], V)
 %
-% This file is part of The Robotics Toolbox for MATLAB (RTB).
-% 
-% RTB is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% RTB is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
-% 
-% You should have received a copy of the GNU Leser General Public License
-% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-% http://www.petercorke.com
-
-%TODO
-% add a 'dt' option, to convert to everything to units of seconds
+%   Return the time to complete the segment at specified velocity. 
 
 function [s,sd,sdd] = lspb(q0, q1, t, V)
 
-    t0 = t;
-    if isscalar(t)
-        t = (0:t-1)';
-    else
-        t = t(:);
-    end
+	if size(t) == [1 1],
+		t = [0:t-1]';
+	end
 
-    tf = max(t(:));
+	tf = max(t(:));
 
-    if nargin < 4
+	if nargin < 4,
         % if velocity not specified, compute it
-        V = (q1-q0)/tf * 1.5;
-    else
-        V = abs(V) * sign(q1-q0);
-        if abs(V) < abs(q1-q0)/tf
-            error('V too small');
-        elseif abs(V) > 2*abs(q1-q0)/tf
-            error('V too big');
-        end
+		V = (q1-q0)/tf * 1.5;
+	else
+		if V < (q1-q0)/tf,
+			error('V too small\n');
+		elseif V > 2*(q1-q0)/tf,
+			error('V too big\n');
+		end
     end
 
-    if q0 == q1
-        s = ones(size(t)) * q0;
-        sd = zeros(size(t));
-        sdd = zeros(size(t));
+    if isempty(t)
+        % return the time to complete, not the path
         return
     end
-
-    tb = (q0 - q1 + V*tf)/V;
-    a = V/tb;
+	tb = (q0 - q1 + V*tf)./V
+	tb
+	a = V./tb;
 
     p = zeros(length(t), 1);
     pd = p;
     pdd = p;
     
-    for i = 1:length(t)
-        tt = t(i);
+	for i = 1:length(t),
+		tt = t(i);
 
-        if tt <= tb
+		if tt <= tb,
             % initial blend
-            p(i) = q0 + a/2*tt^2;
+			p(i) = q0 + a/2*tt^2;
             pd(i) = a*tt;
             pdd(i) = a;
-        elseif tt <= (tf-tb)
+		elseif tt <= (tf-tb),
             % linear motion
-            p(i) = (q1+q0-V*tf)/2 + V*tt;
+			p(i) = (q1+q0-V*tf)/2 + V*tt;
             pd(i) = V;
             pdd(i) = 0;
         else
             % final blend
-            p(i) = q1 - a/2*tf^2 + a*tf*tt - a/2*tt^2;
+			p(i) = q1 - a/2*tf^2 + a*tf*tt - a/2*tt^2;
             pd(i) = a*tf - a*tt;
             pdd(i) = -a;
-        end
-    end
+		end
+	end
 
     switch nargout
         case 0
-            if isscalar(t0)
-                % for scalar time steps, axis is labeled 1 .. M
-                xt = t+1;
-            else
-                % for vector time steps, axis is labeled by vector M
-                xt = t;
-            end
-
             clf
             subplot(311)
-            % highlight the accel, coast, decel phases with different
-            % colored markers
             hold on
             k = t<= tb;
-            plot(xt(k), p(k), 'r-o');
+            plot(t(k), p(k), 'r-o');
             k = (t>=tb) & (t<= (tf-tb));
-            plot(xt(k), p(k), 'b-o');
+            plot(t(k), p(k), 'b-o');
             k = t>= (tf-tb);
-            plot(xt(k), p(k), 'g-o');
-            grid; ylabel('$s$', 'FontSize', 16, 'Interpreter','latex');
-
+            plot(t(k), p(k), 'g-o');
+            grid; ylabel('s');
             hold off
 
             subplot(312)
-            plot(xt, pd); grid; ylabel('$\dot{s}$', 'FontSize', 16, 'Interpreter','latex');
+            plot(t, pd); grid; ylabel('sd');
             
             subplot(313)
-            plot(xt, pdd); grid; ylabel('$\ddot{s}$', 'FontSize', 16, 'Interpreter','latex');
-            if ~isscalar(t0)
-                xlabel('time')
-            else
-                for c=findobj(gcf, 'Type', 'axes')
-                    set(c, 'XLim', [1 t0]);
-                end
-            end
+            plot(t, pdd); grid; ylabel('sdd');
+            xlabel('time')
             shg
         case 1
             s = p;
         case 2
             s = p;
-            sd = pd;
+            sd = [0; diff(s)];
         case 3
             s = p;
-            sd = pd;
-            sdd = pdd;
+            sd = [0; diff(s)];
+            sdd = [0; diff(sd)];
     end
+    
+	if nargout == 0,
+	else
+		tg = p;
+	end
--- tpoly.m
+++ tpoly.m
@@ -1,64 +1,7 @@
-%TPOLY Generate scalar polynomial trajectory
-%
-% [S,SD,SDD] = TPOLY(S0, SF, M) is a scalar trajectory (Mx1) that varies 
-% smoothly from S0 to SF in M steps using a quintic (5th order) polynomial.
-% Velocity and acceleration can be optionally returned as SD (Mx1) and SDD (Mx1).
-%
-% TPOLY(S0, SF, M) as above but plots S, SD and SDD versus time in a single
-% figure.
-%
-% [S,SD,SDD] = TPOLY(S0, SF, T) as above but specifies the trajectory in 
-% terms of the length of the time vector T (Mx1).
-%
-% Notes::
-% - Velocity is in units of distance per trajectory step, not per second.
-% - Acceleration is in units of distance per trajectory step squared, not
-%   per second squared. 
-%
-% Reference:
-%  Robotics, Vision & Control
-%  Chap 3
-%  Springer 2011
-%
-% See also LSPB, JTRAJ.
+function [s,sd,sdd] = tg1(q0, qf, t, qd0, qdf)
 
-
-% Copyright (C) 1993-2015, by Peter I. Corke
-%
-% This file is part of The Robotics Toolbox for MATLAB (RTB).
-% 
-% RTB is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% RTB is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
-% 
-% You should have received a copy of the GNU Leser General Public License
-% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-% http://www.petercorke.com
-
-% [S,SD,SDD] = TPOLY(S0, SF, N, SD0, SDF) as above but specifies initial 
-% and final joint velocity for the trajectory.
-%
-% [S,SD,SDD] = TPOLY(S0, SF, T, SD0, SDF) as above but specifies initial 
-% and final joint velocity for the trajectory and time vector T.
-%
-% Notes::
-% - In all cases if no output arguments are specified S, SD, and SDD are plotted 
-%   against time.
-%
-% See also LSPB, JTRAJ.
-
-function [s,sd,sdd] = tpoly(q0, qf, t, qd0, qdf)
-
-    t0 = t;
     if isscalar(t)
-		t = (0:t-1)';
+        t = [0:(t-1)]';
     else
         t = t(:);
     end
@@ -69,60 +12,38 @@ function [s,sd,sdd] = tpoly(q0, qf, t, qd0, qdf)
         qdf = 0;
     end
     
-    tf = max(t);
+
+    tscal = max(t);
+    t = t / tscal;  % normalized t
     
-    % solve for the polynomial coefficients using least squares
-    X = [
-        0           0           0         0       0   1
-        tf^5        tf^4        tf^3      tf^2    tf  1
-        0           0           0         0       1   0
-        5*tf^4      4*tf^3      3*tf^2    2*tf    1   0
-        0           0           0         2       0   0
-        20*tf^3     12*tf^2     6*tf      2       0   0
-    ];
-    coeffs = (X \ [q0 qf qd0 qdf 0 0]')';
+    A = 6*(qf - q0) - 3*(qdf+qd0)*tscal;
+    B = -15*(qf - q0) + (8*qd0 + 7*qdf)*tscal;
+    C = 10*(qf - q0) - (6*qd0 + 4*qdf)*tscal;
+    D = 0;
+    E = qd0*tscal; % as the t vector has been normalized
+    F = q0;
 
-    % coefficients of derivatives 
-    coeffs_d = coeffs(1:5) .* (5:-1:1);
-    coeffs_dd = coeffs_d(1:4) .* (4:-1:1);
+    coeffs = [A B C D E F];
 
-    % evaluate the polynomials
     p = polyval(coeffs, t);
-    pd = polyval(coeffs_d, t);
-    pdd = polyval(coeffs_dd, t);
+    pd = polyval([5*A 4*B 3*C 2*D E], t) / tscal;
+    pdd = polyval([20*A 12*B 6*C 2*D], t) / tscal^2;
 
     switch nargout
         case 0
-            if isscalar(t0)
-                % for scalar time steps, axis is labeled 1 .. M
-                xt = t+1;
-            else
-                % for vector time steps, axis is labeled by vector M
-                xt = t;
-            end
-
-            clf
             subplot(311)
-            plot(xt, p); grid; ylabel('$s$', 'FontSize', 16, 'Interpreter','latex');
-
+            plot(t, p); grid; ylabel('s');
             subplot(312)
-            plot(xt, pd); grid; ylabel('$\dot{s}$', 'FontSize', 16, 'Interpreter','latex');
-            
+            plot(t, pd); grid; ylabel('sd');
             subplot(313)
-            plot(xt, pdd); grid; ylabel('$\ddot{s}$', 'FontSize', 16, 'Interpreter','latex');
-            if ~isscalar(t0)
-                xlabel('time')
-            else
-                for c=findobj(gcf, 'Type', 'axes')
-                    set(c, 'XLim', [1 t0]);
-                end
-            end
+            plot(t, pdd); grid;  ylabel('sdd');
+            xlabel('time')
             shg
         case 1
             s = p;
         case 2
             s = p;
-            sd = pd;
+            sd = pd
         case 3
             s = p;
             sd = pd;
