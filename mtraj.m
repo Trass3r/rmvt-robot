0c3a3925e4b989074e365514d24c73da775c222f
pic <pic@gmail.com>
2009-07-16 08:34:34 +0000

    Return sd and sdd as well.
    r278 matlab/branches/book

--- mtraj.m
+++ mtraj.m
@@ -1,12 +1,24 @@
-function tg = mtraj(tfunc, q0, qf, N)
+function [s,Sd,Sdd] = mtraj(tfunc, q0, qf, N)
 
+    if ~isscalar(N)
+        N = length(N);
+    end
     if numcols(q0) ~= numcols(qf)
         error('must be same number of columns in q0 and qf')
     end
 
-    tg = zeros(N, numcols(q0));
+    s = zeros(N, numcols(q0));
+    sd = zeros(N, numcols(q0));
+    sdd = zeros(N, numcols(q0));
 
     for i=1:numcols(q0)
         % for each axis
-        tg(:,i) = tfunc(q0(i), qf(i), N);
+        [s(:,i),sd(:,i),sdd(:,i)] = tfunc(q0(i), qf(i), N);
+    end
+
+    if nargout > 1
+        Sd = sd;
+    end
+    if nargout > 2
+        Sdd = sdd;
     end
