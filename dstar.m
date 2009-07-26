809ebebf40d53e0c528e7dc69e2e90c42ab96867
pic <pic@gmail.com>
2009-07-26 10:40:51 +0000

    Working version prior to refactor with navigate superclass.
    r281 matlab/branches/book

--- dstar.m
+++ dstar.m
@@ -7,17 +7,30 @@
 classdef dstar < handle
 
     properties
+        occgrid
+
+        costmap   % world cost map
+
+        % info kept per cell (state)
         b       % backpointer (0 means not set)
-        c       % cost
         h       % pathcost
+
+        % info for the open list
+        openlist    % list of open states
+            % 3xN matrix: state index;  k
         k_old
         k_min
-        k
+        %k
+        G       % goal
         t       % tag: NEW OPEN CLOSED
-        openlist    % list of open states
-            % 3xN matrix: state index; h; k
-        G
 
+        openlist_maxlen
+        niter
+        navhook
+
+        verbose
+
+        % tag state values
         NEW = 0;
         OPEN = 1;
         CLOSED = 2;
@@ -25,138 +38,171 @@ classdef dstar < handle
 
     methods
 
-        function kk = k(X)
-            i = find(openlist(1,:) == X);
-            kk = openlist(3,i);
-        end
+        % constructor
+        function ds = dstar(costmap, goal)
+            ds.costmap = costmap;
+            ds.b = zeros(size(costmap), 'uint32');
+            ds.t = zeros(size(costmap), 'uint8');
+            ds.h = Inf*ones(size(costmap));
+            ds.openlist = zeros(2,0);
+            ds.verbose = false;
+            ds.navhook = [];
 
-        function hh = h(X)
-            i = find(openlist(1,:) == X);
-            hh = openlist(2,i);
-        end
+            ds.openlist_maxlen = -Inf;
+            ds.niter = 0;
 
-        function k_set(X, k_new)
-            disp('set k');
-            i = find(openlist(1,:) == X);
-            openlist(3,i) = k_new;
-        end
-
-        function h_set(X, h_new)
-            disp('set h');
-            i = find(openlist(1,:) == X);
-            openlist(2,i) = h_new;
-        end
-
-        function d = dstar(c)
-            d.b = zeros(size(c), 'uint32');
-            d.c = c;
-            d.t = zeros(size(c), 'uint8');
-        end
-
-        function setgoal(i,j)
-            G = sub2ind(size(c), i, j);
-            insert(G, 0);
-        end
-
-        function insert(X, h_new)
-            i = find(openlist(1,:) == X);
-            if length(i) > 0
-                error( sprintf('d*:insert: state %d already exists', X) );
+            if nargin >= 2
+                ds.setgoal(goal);
             end
+        end
 
-            if t(X) == NEW
-                k_new = h_new;
-            elseif t(X) == OPEN
-                k_new = min( k(X), h_new );
-            elseif t(X) == CLOSED
-                k_new = min( h(X), h_new );
+        function costmap_set(ds, costmap)
+            ds.costmap = costmap;
+        end
+
+        function c = costmap_get(ds)
+            c = ds.costmap;
+        end
+
+        function occgrid_set(ds, og, cost)
+            if nargin < 3
+                cost = 1;
             end
-
-            % add a new column to the open list
-            openlist = [openlist [X; h_new; k_new]];
-            t(X) = OPEN;
+            costmap = og;
+            costmap(costmap==0) = cost;     % unoccupied cells have driving cost
+            costmap(costmap==1) = Inf;      % occupied cells have Inf driving cost
         end
 
-        function delete(X)
-            i = find(openlist(1,:) == X);
-            if length(i) ~= 1
-                error( sprintf('d*:delete: state %d doesnt exist', X) );
+        function verbosity(ds, v)
+            ds.verbose = v;
+        end
+            
+        function s = display(ds)
+            s = char(ds);
+        end
+
+        function s = char(ds)
+            s = sprintf('D*: open list %d\n', numcols(ds.openlist));
+        end
+
+        function setgoal(ds, goal)
+            ds.G = sub2ind(size(ds.costmap), goal(2), goal(1));
+            if ds.costmap(ds.G) == Inf
+                error('cant set goal inside obstacle');
             end
-            openlist(:,i) = []; % remove the column
-            t(X) = CLOSED;
+            ds.INSERT(ds.G, 0);
+            ds.h(ds.G) = 0;
         end
 
-        function ms = min_state()
-            if length(openlist) == 0
-                ms = [];
-            end
-            [kmin,i] = min(k(openlist(3,:)));
-            ms = openlist(1,i);
+        function navhook_set(ds, navhook)
+            ds.navhook = navhook
         end
-
-        function kmin = get_kmin()
-            kmin = min(k(openlist(3,:)));
-        end
-
-        % return index of neighbour states as a row vector
-        function Y = neighbours(X)
-            [r,c] = ind2sub(size(c), X);
-
-            % 4-way neighbours for now
-            Y = [];
-            Y = [Y sub2ind(size(c), r-1, c)];
-            Y = [Y sub2ind(size(c), r+1, c)];
-            Y = [Y sub2ind(size(c), r, c-1)];
-            Y = [Y sub2ind(size(c), r, c+1)];
-        end
-
-        function plan()
+            
+        function p = navigate(ds, robot, varargin)
+            p = robot;
+            navhook = if ds.navhook
             while true
-                if process_state() < 0
-                    break;
+                plot(robot(1), robot(2), 'g.');
+                drawnow 
+                robot = ds.next(robot);
+                if isempty(robot)
+                    break
+                end
+                p = [p; robot];
+                if isa('function_handle', ds.navhook)
+                    ds.navhook(ds, robot(1), robot(2));
                 end
             end
         end
 
-        function r = process_state()
-            X = min_state();
+        function n = next(ds, current)
+            X = sub2ind(size(ds.costmap), state(2), state(1));
+            X = ds.b(X);
+            if X == 0
+                n = [];
+            else
+                [r,c] = ind2sub(size(ds.costmap), X);
+                n = [c,r];
+            end
+        end
 
-            if isempty(X)
+        function reset(ds)
+            ds.t = zeros(size(costmap), 'uint8');
+        end
+
+        function plan(ds)
+            ds.niter = 0;
+            while true
+                ds.niter = ds.niter + 1;
+
+                if ds.PROCESS_STATE() < 0
+                    break;
+                end
+                pause
+                if ds.verbose
+                    disp(' ')
+                end
+            end
+        end
+
+        function modify_cost(ds, point, newcost)
+            X = sub2ind(size(ds.costmap), point(2), point(1));
+            ds.costmap(X) = newcost;
+            if ds.t(X) == ds.CLOSED
+                ds.INSERT(X, ds.h(X));
+            end
+
+        end
+
+        % The main D* function as per the Stentz paper, comments Ln are the original
+        % line numbers.
+        function r = PROCESS_STATE(d)
+            X = d.MIN_STATE();                          % L1
+
+            if isempty(X)                               % L2
                 r = -1;
                 return;
             end
 
-            k_old = get_kmin(d); delete(X);
+            k_old = d.GET_KMIN(); d.DELETE(X);          % L3
 
-            if k_old < h(X)
-                % for each neighbour Y of X
-                for Y=neighbours(X)
-                    if (h(Y) <= k_old) && (h(X) > h(Y)+c(Y,X))
-                        b(X) = Y; h_set(X,  h(Y) + c(Y,X) );
+            if k_old < d.h(X)                           % L4
+                if ds.verbose
+                    fprintf('k_old < h(X):  %f %f\n', k_old, d.h(X));
+                end
+                for Y=d.neighbours(X)                   % L5
+                    if (d.h(Y) <= k_old) && (d.h(X) > d.h(Y)+d.c(Y,X))  % L6
+                        d.b(X) = Y;
+                        d.h(X) = d.h (Y) + d.c(Y,X);                    % L7
                     end
                 end
-            elseif k_old == h(X)
-                % for each neighbour Y of X
-                for Y=neighbours(X)
-                    if (t(Y) == NEW) || ...
-                            ( (b(Y) == X) && (h(Y) ~= (h(X) + c(X,Y))) ) || ..
-                            ( (b(Y) ~= X) && (h(Y) > (h(X) + c(X,Y))) )
-                        b(Y) = X; insert(Y, h(X)+c(X,Y));
+            end
+
+            if k_old == d.h(X)                          % L8
+                if d.verbose
+                    fprintf('k_old == h(X): %f\n', k_old);
+                end
+                for Y=d.neighbours(X)                   % L9
+                    if (d.t(Y) == d.NEW) || ...                         % L10-12
+                            ( (d.b(Y) == X) && (d.h(Y) ~= (d.h(X) + d.c(X,Y))) ) || ...
+                            ( (d.b(Y) ~= X) && (d.h(Y) > (d.h(X) + d.c(X,Y))) )
+                        d.b(Y) = X; d.INSERT(Y, d.h(X)+d.c(X,Y));       % L13
                     end
                  end
-            else
-                % for each neighbour Y of X
-                for Y=neighbours(X)
-                    if (t(Y) == NEW) || ( (b(Y) == X) && (h(Y) ~= (h(X) + c(X,Y))) )
-                        b(Y) = X; insert(Y, h(X)+c(X,Y));
+            else                                        % L14
+                if d.verbose
+                    disp('k_old > h(X)');
+                end
+                for Y=d.neighbours(X)                   % L15
+                    if (d.t(Y) == d.NEW) || ( (d.b(Y) == X) && (d.h(Y) ~= (d.h(X) + d.c(X,Y))) )
+                        d.b(Y) = X; d.INSERT(Y, d.h(X)+d.c(X,Y));   % L18
                     else
-                        if ( (b(Y) ~= X) && (h(Y) > (h(X) + c(X,Y))) )
-                            insert(X, h(X));
+                        if ( (d.b(Y) ~= X) && (d.h(Y) > (d.h(X) + d.c(X,Y))) )
+                            d.INSERT(X, d.h(X));                    % L21
                         else
-                            if (b(Y) ~= X) && (h(X) > (h(Y) + c(Y,X))) && ...
-                                    (t(Y) == CLOSED) && h(Y) > k_old
-                                insert(Y, h(Y));
-
+                            if (d.b(Y) ~= X) && (d.h(X) > (d.h(Y) + d.c(Y,X))) && ...
+                                    (d.t(Y) == d.CLOSED) && d.h(Y) > k_old
+                                d.INSERT(Y, d.h(Y));                % L25
                             end
                         end
                     end
@@ -167,5 +213,86 @@ classdef dstar < handle
             return;
         end % process_state(0
 
-    end % methods
+        function kk = k(X)
+            i = find(ds.openlist(1,:) == X);
+            kk = ds.openlist(2,i);
+        end
+
+        function INSERT(ds, X, h_new)
+            if ds.verbose
+                fprintf('insert %d = %f\n', X, h_new);
+            end
+
+            i = find(ds.openlist(1,:) == X);
+            if length(i) > 1
+                error( sprintf('d*:INSERT: state in open list %d times', X) );
+            end
+
+            if ds.t(X) == ds.NEW
+                k_new = h_new;
+                % add a new column to the open list
+                ds.openlist = [ds.openlist [X; k_new]];
+            elseif ds.t(X) == ds.OPEN
+                k_new = min( ds.openlist(2,i), h_new );
+            elseif ds.t(X) == ds.CLOSED
+                k_new = min( ds.h(X), h_new );
+                % add a new column to the open list
+                ds.openlist = [ds.openlist [X; k_new]];
+            end
+
+            if numcols(ds.openlist) > ds.openlist_maxlen
+                ds.openlist_maxlen = numcols(ds.openlist);
+            end
+
+            ds.h(X) = h_new;
+            ds.t(X) = ds.OPEN;
+        end
+
+        function DELETE(ds, X)
+            if ds.verbose
+                fprintf('delete %d\n', X);
+            end
+            i = find(ds.openlist(1,:) == X);
+            if length(i) ~= 1
+                error( sprintf('d*:DELETE: state %d doesnt exist', X) );
+            end
+            ds.openlist(:,i) = []; % remove the column
+            ds.t(X) = ds.CLOSED;
+        end
+
+        function ms = MIN_STATE(ds)
+            if length(ds.openlist) == 0
+                ms = [];
+            end
+            [kmin,i] = min(ds.openlist(2,:));
+            %[kmin,i] = min(ds.k(ds.openlist(1,:)));
+            ms = ds.openlist(1,i);
+        end
+
+        function kmin = GET_KMIN(ds)
+            kmin = min(ds.openlist(2,:));
+        end
+
+        function cost = c(ds, X, Y)
+            [r,c] = ind2sub(size(ds.costmap), [X; Y]);
+            dist = sqrt(sum(diff([r c]).^2));
+            dcost = (ds.costmap(X) + ds.costmap(Y))/2;
+
+            cost = dist * dcost;
+        end
+
+        % return index of neighbour states as a row vector
+        function Y = neighbours(ds, X)
+            dims = size(ds.costmap);
+            [r,c] = ind2sub(dims, X);
+
+            % list of 8-way neighbours
+            Y = [r-1 r-1 r-1 0 0  r+1 r+1 r+1; c-1 c c+1 c-1 c+1 c-1 c c+1];
+            k = (min(Y)>0) & (Y(1,:)<=dims(1)) & (Y(2,:)<=dims(2));
+            Y = Y(:,k);
+
+            Y = sub2ind(dims, Y(1,:)', Y(2,:)')';
+        end
+
+    end % method
 end % classdef
