620bb344cdb60cc06e2afa04ec9d845fe811b9f3
pic <pic@gmail.com>
2010-03-28 02:53:13 +0000

    Robot model names now prefixed by mdl_
    r408 matlab/branches/book

--- mdl_twolink.m
+++ mdl_twolink.m
@@ -1,29 +1,24 @@
-%MDL_TWOLINK Create model of a 2-link mechanism
+%TWOLINK Load kinematic and dynamic data for a simple 2-link mechanism
 %
-% MDL_TWOLINK is a script that creates the workspace variable tl which
-% describes the kinematic and dynamic characteristics of a simple planar
-% 2-link mechanism.
+%	TWOLINK
 %
-% Also defines the vector:
-%   qz   corresponds to the zero joint angle configuration.
+% Defines the object 'tl' in the current workspace which describes the 
+% kinematic and dynamic characterstics of a simple planar 2-link mechanism.
 %
-% Notes::
-% - SI units are used.
-% - It is a planar mechanism operating in the XY (horizontal) plane and is 
-%   therefore not affected by gravity.
-% - Assume unit length links with all mass (unity) concentrated at the joints.
+% Example based on Fig 3-6 (p73) of Spong and Vidyasagar (1st edition).  
+% It is a planar mechanism operating in the XY (horizontal) plane and is 
+% therefore not affected by gravity.
 %
-% References::
-%  - Based on Fig 3-6 (p73) of Spong and Vidyasagar (1st edition).  
+% Assume unit length links with all mass (unity) concentrated at the joints.
 %
-% See also SerialLink, mdl_onelink, mdl_twolink_mdh, mdl_planar2.
+% Also define the vector qz = [0 0] which corresponds to the zero joint
+% angle configuration.
+%
+% See also: DH, DYN, PUMA560, PUMA560AKB, STANFORD.
 
-% MODEL: generic, planar, dynamics, 2DOF, standard_DH
-
-
-% Copyright (C) 1993-2015, by Peter I. Corke
+% Copyright (C) 2000-2008, by Peter I. Corke
 %
-% This file is part of The Robotics Toolbox for MATLAB (RTB).
+% This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
 % RTB is free software: you can redistribute it and/or modify
 % it under the terms of the GNU Lesser General Public License as published by
@@ -37,21 +32,12 @@
 % 
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-% http://www.petercorke.com
 
+twolink_dh = [
+% theta d a alpha a	sigma	m	rx	ry	rz	Ixx	Iyy	Izz	Ixy	Iyz	Ixz	Jm	G
+  0     0         1     0         0     1       1       0       0       0       0       0       0       0       0        0      1
+  0     0         1     0         0     1       1       0       0       0       0       0       0       0       0        0      1
+];
 
-a1 = 1;
-a2 = 1;
-%   theta d a alpha
-
-twolink = SerialLink([
-    Revolute('d', 0, 'a', a1, 'alpha', 0, 'm', 1, 'r', [-0.5 0 0], 'I', [0 0 0], 'B', 0, 'G', 0, 'Jm', 0, 'standard')
-    Revolute('d', 0, 'a', a2, 'alpha', 0, 'm', 1, 'r', [-0.5 0 0], 'I', [0 0 0], 'B', 0, 'G', 0, 'Jm', 0, 'standard')
-    ], ...
-    'name', 'two link', ...
-    'comment', 'from Spong, Hutchinson, Vidyasagar');
+two_link = SerialLink(twolink_dh, 'name', 'Simple two link');
 qz = [0 0];
-qn = [pi/6, -pi/6];
-twolink.base = trotx(pi/2);
-twolink.fast = 0;
