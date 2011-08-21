e19decc5d890c498b7a50eb004d1e7a7904ca6f0
pic <pic@gmail.com>
2011-08-21 11:16:30 +0000

    use option parser, update calls to homtrans
    r541 matlab/branches/book

--- trplot.m
+++ trplot.m
@@ -1,23 +1,24 @@
-%TRPLOT Plot a transformation
+%TRPLOT Draw a coordinate frame
 %
-%   TRPLOT(T)
-%   TRPLOT(T, name)
-%   TRPLOT(T, name, color)
+% TRPLOT(T, OPTIONS) draws a 3D coordinate frame represented by the homogeneous 
+% transform T.
 %
-% In a set of axes draw a coordinate frame.  The frame can optionally
-% be named and have a specified color.
+% TRPLOT(R, OPTIONS) draws a 3D coordinate frame represented by the orthonormal
+% rotation matrix R.
 %
-% Name can be a string which is appears in the axis labels, X<name>, etc
-%   eg. trplot(trotx(.2), 'cam')
+% Options::
+% 'color', c         The color to draw the axes, Matlab colorspec.
+% 'axes'             Set dimensions of the Matlab axes
+% 'frame', f         The name which appears on the axis labels and the frame itself
+% 'text_opts', opt   A cell array of Matlab text properties
+% 'arrow'            Use arrows rather than line segments for the axes
+% 'width', w         Width of arrow tips
+% 'handle', h        Draw in the Matlab axes specified by h
 %
-% Name can be a cell array {axname, framename}, axname acts the same way
-% as above, framename is written near the origin.  We can use LaTeX math
-% mode.  For axname include a %c where the axis name (X, Y, Z) will be
-% substituted  
-%  eg.  trplot(trotx(.2), {'$%c_{cam}$','$\{X_{cam}\}$'})
-%
+% See also TRPLOT2, TRANIMATE.
 
-% Copyright (C) 2008, by Peter I. Corke
+
+% Copyright (C) 1993-2011, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
@@ -34,47 +35,27 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 
+% TODO
+%  need to decide how to handle scaling
+%  what does hold on mean?  don't touch scaling?
+
 function hout = trplot(T, varargin)
 
     opt.color = 'b';
     opt.axes = true;
-    opt.name = [];
     opt.axis = [];
-    opt.framename = [];
-    opt.axlabel = [];
-    opt.text_opts = {};
+    opt.frame = [];
+    opt.text_opts = [];
     opt.view = [];
     opt.width = 1;
     opt.arrow = false;
-    argc = 1;
-    while argc <= length(varargin)
-        switch lower(varargin{argc})
-        case 'color'
-            opt.color = varargin{argc+1}; argc = argc+1;
-        case 'frame'
-            opt.framename = varargin{argc+1}; argc = argc+1;
-        case 'label'
-            opt.axlabel = varargin{argc+1}; argc = argc+1;
-        case 'noaxes'
-            opt.axes = false;
-        case 'arrow'
-            opt.arrow = true;
-        case 'unit'
-            opt.axis = [-1 1 -1 1 -1 1]*1.2';
-        case 'axis'
-            opt.axis = varargin{argc+1}; argc = argc+1;
-        case 'textopts'
-            opt.text_opts = varargin{argc+1}; argc = argc+1;
-        case 'width'
-            opt.width = varargin{argc+1}; argc = argc+1;
-        case 'view'
-            opt.view = varargin{argc+1}; argc = argc+1;
-        otherwise
-            error( sprintf('unknown option <%s>', varargin{argc}));
-        end
-        argc = argc + 1;
-    end
+    opt.handle = [];
 
+    opt = tb_optparse(opt, varargin);
+
+    if isempty(opt.text_opts)
+        opt.text_opts = {};
+    end
     if isempty(opt.axis)
         if all(size(T) == [3 3]) || norm(transl(T)) < eps
             opt.axis = [-1 1 -1 1 -1 1]*1.2';
@@ -84,43 +65,50 @@ function hout = trplot(T, varargin)
     
     % TODO: should do the 2D case as well
     
-    ih = ishold;
-    if ~ih
-        % if hold is not on, then clear the axes and set scaling
-		cla
-        if ~isempty(opt.axis)
-            axis(opt.axis);
-        end
-        daspect([1 1 1]);
-        
-        if opt.axes
-            xlabel( 'X');
-            ylabel( 'Y');
-            zlabel( 'Z');
-            rotate3d on
-        end
-        new_plot = true;
+    if ~isempty(opt.handle)
+        hax = opt.handle;
+        hold(hax);
+        hax
     else
-        set(gca, 'XLimMode', 'auto');
-        set(gca, 'YLimMode', 'auto');
-        set(gca, 'ZLimMode', 'auto');
-	end
+        ih = ishold;
+        if ~ih
+            % if hold is not on, then clear the axes and set scaling
+            cla
+            if ~isempty(opt.axis)
+                axis(opt.axis);
+            end
+            daspect([1 1 1]);
+            
+            if opt.axes
+                xlabel( 'X');
+                ylabel( 'Y');
+                zlabel( 'Z');
+                rotate3d on
+            end
+            new_plot = true;
+        else
+            %set(gca, 'XLimMode', 'auto');
+            %set(gca, 'YLimMode', 'auto');
+            %set(gca, 'ZLimMode', 'auto');
+        end
+        hax = gca;
+        hold on
+    end
 
     opt.text_opts = {opt.text_opts{:}, 'Color', opt.color};
 
-	hold on
 
-    % trplot( Q.r, fmt, color);
-    if size(T) == [3 3],
+    % trplot( Q.R, fmt, color);
+    if size(T) == [3 3]
         T = r2t(T);
     end
 
 
-	% create unit vectors
-	o =  transformp(T, [0 0 0]');
-	x1 = transformp(T, [1 0 0]');
-	y1 = transformp(T, [0 1 0]');
-	z1 = transformp(T, [0 0 1]');
+    % create unit vectors
+    o =  homtrans(T, [0 0 0]');
+    x1 = homtrans(T, [1 0 0]');
+    y1 = homtrans(T, [0 1 0]');
+    z1 = homtrans(T, [0 0 1]');
 
     
     % draw the axes
@@ -128,7 +116,11 @@ function hout = trplot(T, varargin)
     mstart = [o o o]';
     mend = [x1 y1 z1]';
     
-    hg = hgtransform;
+    if isempty(opt.handle)
+        hg = hgtransform;
+    else
+        hg = hax;
+    end
 
     if opt.arrow
         % draw the 3 arrows
@@ -138,45 +130,41 @@ function hout = trplot(T, varargin)
             set(h, 'Parent', hg);
         end
     else
-        for i=1:3,
-            h = plot2([mstart(i,1:3); mend(i,1:3)]);
-            set(h, 'Parent', hg);
+        for i=1:3
+            h = plot2([mstart(i,1:3); mend(i,1:3)], 'Color', opt.color, 'Parent', hg);
 
         end
     end
 
     % label the axes
-    if isempty(opt.framename),
+    if isempty(opt.frame)
         fmt = '%c';
     else
-%             if opt.axlabel(1) == '$',
+%             if opt.axlabel(1) == '$'
 %                 fmt = axlabel;
 %                 opt.text_opts = {opt.text_opts{:}, 'Interpreter', 'latex'};
-%             else,
+%             else
 %                 fmt = sprintf('%%c%s', axlabel);
 %             end
-        fmt = sprintf('%%c_{%s}', opt.framename);
+        fmt = sprintf('%%c_{%s}', opt.frame);
     end
     
     % add the labels to each axis
-	h = text(x1(1), x1(2), x1(3), sprintf(fmt, 'X'));
-	set(h, opt.text_opts{:});
-    set(h, 'Parent', hg);
+    h = text(x1(1), x1(2), x1(3), sprintf(fmt, 'X'), 'Parent', hg);
+    set(h, opt.text_opts{:});
    
 
-	h = text(y1(1), y1(2), y1(3), sprintf(fmt, 'Y'));
-	set(h, opt.text_opts{:});
-        set(h, 'Parent', hg);
+    h = text(y1(1), y1(2), y1(3), sprintf(fmt, 'Y'), 'Parent', hg);
+    set(h, opt.text_opts{:});
     
 
-	h = text(z1(1), z1(2), z1(3), sprintf(fmt, 'Z'));
-	set(h, opt.text_opts{:});
-        set(h, 'Parent', hg);
+    h = text(z1(1), z1(2), z1(3), sprintf(fmt, 'Z'), 'Parent', hg);
+    set(h, opt.text_opts{:});
     
     % label the frame
-    if ~isempty(opt.framename),
+    if ~isempty(opt.frame)
         h = text(o(1)-0.04*x1(1), o(2)-0.04*y1(2), o(3)-0.04*z1(3), ...
-            ['\{' opt.framename '\}']);
+            ['\{' opt.frame '\}'], 'Parent', hax);
         set(h, 'VerticalAlignment', 'middle', ...
             'HorizontalAlignment', 'center', opt.text_opts{:});
     end
@@ -190,9 +178,9 @@ function hout = trplot(T, varargin)
     elseif ~isempty(opt.view)
         view(opt.view);
     end
-	grid on
-	if ~ih
-		hold off
+    if isempty(opt.handle) && ~ih
+        grid on
+        hold off
     end
     
     if nargout > 0
