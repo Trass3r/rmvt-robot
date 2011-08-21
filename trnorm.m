96470f5e999669da6bdb232cbd17bb71705aeb0d
pic <pic@gmail.com>
2011-08-21 11:14:43 +0000

    handle rotation matrices as well
    r540 matlab/branches/book

--- trnorm.m
+++ trnorm.m
@@ -1,17 +1,18 @@
-%TRNORM Normalize a homogeneous transformation.
+%TRNORM Normalize a homogeneous transform
 %
-%	TN = TRNORM(T) 
-%
-% Returns a normalized homogeneous tranformation matrix in which the rotation
-% submatrix is a proper orthogonal matrix.
-% The O and V vectors are normalized and the normal vector is formed from
+% TN = TRNORM(T) is a normalized homogeneous transformation matrix in which 
+% the rotation submatrix is guaranteed to be a proper orthogonal matrix.
+% The O and A vectors are normalized and the normal vector is formed from
 % O x A.
 %
-% Finite word length arithmetic can cause transforms to become `unnormalized'.
+% Notes::
+% - Used to prevent finite word length arithmetic causing transforms to 
+%  become `unnormalized'.
 %
-% See also: OA2TR
+% See also OA2TR.
 
-% Copyright (C) 1993-2008, by Peter I. Corke
+
+% Copyright (C) 1993-2011, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
@@ -33,12 +34,17 @@ function r = trnorm(t)
     if ndims(t) == 3
         nd = size(t, 3);
         r = zeros(4,4,nd);
-        for i=1:nd,
+        for i=1:nd
             r(:,:,i) = trnorm(t(:,:,i));
         end
         return
     end
 
+    if all(size(t) == [4 4])
 	n = cross(t(1:3,2), t(1:3,3));	% N = O x A
     o = cross(t(1:3,3), n);         % O = A x N
 	r = [unit(n) unit(t(1:3,2)) unit(t(1:3,3)) t(1:3,4); 0 0 0 1];
+    elseif all(size(t) == [3 3])
+            r = t;
+        end
+
