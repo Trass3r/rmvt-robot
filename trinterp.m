9fd7154a400c667a1968f21a8ce37cbaf906d8dc
pic <pic@gmail.com>
2011-08-21 11:14:08 +0000

    better handle trajectory, quaternion to Quaternion
    r539 matlab/branches/book

--- trinterp.m
+++ trinterp.m
@@ -1,19 +1,18 @@
 %TRINTERP Interpolate homogeneous transformations
 %
-%	TR = TRINTERP(T0, T1, r)
+% T = TRINTERP(T0, T1, S) is a homogeneous transform interpolation 
+% between T0 when S=0 to T1 when S=1.  Rotation is interpolated using 
+% quaternion spherical linear interpolation.  If S is an N-vector then
+% T is a 4x4xN matrix where the transform T(:,:,i) corresponds to S(i).
 %
-% Returns a homogeneous transform interpolation between T0 and T1 as
-% r varies from 0 to 1.  Rotation is interpolated using quaternion
-% spherical linear interpolation.
+% T = TRINTERP(T, S) is a transform that varies from the identity matrix when
+% S=0 to T when R=1. If S is an N-vector then T is a 4x4xN matrix where 
+% the transform T(:,:,i) corresponds to S(i).
 %
-%	TR = TRINTERP(T, r)
-%
-% Returns a transform that varies from the identity matrix, r=0, to T
-% when r=1.
-%
-% See also: CTRAJ, QUATERNION
+% See also CTRAJ, QUATERNION.
 
-% Copyright (C) 1993-2008, by Peter I. Corke
+
+% Copyright (C) 1993-2011, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
@@ -30,21 +29,43 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function t = trinterp(T0, T1, r)
+function T = trinterp(A, B, C)
 
-    if nargin == 3,
-        q0 = quaternion(T0);
-        q1 = quaternion(T1);
+    if nargin == 3
+        %	TR = TRINTERP(T0, T1, r)
+        T0 = A; T1 = B; r = C;
+
+        if length(r) > 1
+            T = [];
+            for rr=r(:)'
+                TT = trinterp(T0, T1, rr);
+                T = cat(3, T, TT);
+            end
+            return;
+        end
+
+        q0 = Quaternion(T0);
+        q1 = Quaternion(T1);
 
         p0 = transl(T0);
         p1 = transl(T1);
 
         qr = q0.interp(q1, r);
         pr = p0*(1-r) + r*p1;
-    elseif nargin == 2,
-        r = T1;
-        
-        q0 = quaternion(T0);
+    elseif nargin == 2
+    %	TR = TRINTERP(T, r)
+        T0 = A; r = B;
+
+        if length(r) > 1
+            T = [];
+            for rr=r(:)'
+                TT = trinterp(T0, rr);
+                T = cat(3, T, TT);
+            end
+            return;
+        end
+
+        q0 = Quaternion(T0);
         p0 = transl(T0);
 
         qr = q0.scale(r);
@@ -52,5 +73,5 @@ function t = trinterp(T0, T1, r)
     else
         error('must be 2 or 3 arguments');
     end
-    t = [qr.r pr; 0 0 0 1];
+    T = rt2tr(qr.R, pr);
         
