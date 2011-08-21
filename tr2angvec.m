36d825cde921e793dbc28c461614ad7452d6e56d
pic <pic@gmail.com>
2011-08-21 11:08:56 +0000

    code tidyup, angle is between +/- pi/2
    r533 matlab/branches/book

--- tr2angvec.m
+++ tr2angvec.m
@@ -1,14 +1,18 @@
-%TR2ANGVEC Convert to angle/vector form
+%TR2ANGVEC Convert rotation matrix to angle-vector form
 %
-% 	[THETA V] = TR2ANGVEC(M)
+% [THETA,V] = TR2ANGVEC(R) is a rotation of THETA about the vector V 
+% equivalent to the orthonormal rotation matrix R.
 %
-% Returns a vector/angle representation of the pose corresponding to M, either a rotation
-% matrix or the rotation part of a homogeneous transform.
-% This is a rotation of THETA about the vector V.
+% [THETA,V] = TR2ANGVEC(T) is a rotation of THETA about the vector V 
+% equivalent to the rotational component of the homogeneous transform T.
 %
-% See also: ANGVEC2R, ANGVEC2TR
+% Notes::
+% - If no output arguments are specified the result is displayed.
+%
+% See also ANGVEC2R, ANGVEC2TR.
 
-% Copyright (C) 1993-2008, by Peter I. Corke
+
+% Copyright (C) 1993-2011, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for Matlab (RTB).
 % 
@@ -25,29 +29,33 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function [theta, v] = tr2angvec(t)
+function [theta, v] = tr2angvec(R)
 
-    t = t2r(t);
+    if ~isrot(R)
+        R = t2r(R);
+    end
     
-	qs = sqrt(trace(t)+1)/2.0;
-	kx = t(3,2) - t(2,3);	% Oz - Ay
-	ky = t(1,3) - t(3,1);	% Ax - Nz
-	kz = t(2,1) - t(1,2);	% Ny - Ox
+	qs = sqrt(trace(R)+1)/2.0;
+    qs = min(qs, 1);
+        
+	kx = R(3,2) - R(2,3);	% Oz - Ay
+	ky = R(1,3) - R(3,1);	% Ax - Nz
+	kz = R(2,1) - R(1,2);	% Ny - Ox
 
-	if (t(1,1) >= t(2,2)) & (t(1,1) >= t(3,3)) 
-		kx1 = t(1,1) - t(2,2) - t(3,3) + 1;	% Nx - Oy - Az + 1
-		ky1 = t(2,1) + t(1,2);			% Ny + Ox
-		kz1 = t(3,1) + t(1,3);			% Nz + Ax
+	if (R(1,1) >= R(2,2)) && (R(1,1) >= R(3,3)) 
+		kx1 = R(1,1) - R(2,2) - R(3,3) + 1;	% Nx - Oy - Az + 1
+		ky1 = R(2,1) + R(1,2);			% Ny + Ox
+		kz1 = R(3,1) + R(1,3);			% Nz + Ax
 		add = (kx >= 0);
-	elseif (t(2,2) >= t(3,3))
-		kx1 = t(2,1) + t(1,2);			% Ny + Ox
-		ky1 = t(2,2) - t(1,1) - t(3,3) + 1;	% Oy - Nx - Az + 1
-		kz1 = t(3,2) + t(2,3);			% Oz + Ay
+	elseif (R(2,2) >= R(3,3))
+		kx1 = R(2,1) + R(1,2);			% Ny + Ox
+		ky1 = R(2,2) - R(1,1) - R(3,3) + 1;	% Oy - Nx - Az + 1
+		kz1 = R(3,2) + R(2,3);			% Oz + Ay
 		add = (ky >= 0);
 	else
-		kx1 = t(3,1) + t(1,3);			% Nz + Ax
-		ky1 = t(3,2) + t(2,3);			% Oz + Ay
-		kz1 = t(3,3) - t(1,1) - t(2,2) + 1;	% Az - Nx - Oy + 1
+		kx1 = R(3,1) + R(1,3);			% Nz + Ax
+		ky1 = R(3,2) + R(2,3);			% Oz + Ay
+		kz1 = R(3,3) - R(1,1) - R(2,2) + 1;	% Az - Nx - Oy + 1
 		add = (kz >= 0);
 	end
 
@@ -62,6 +70,10 @@ function [theta, v] = tr2angvec(t)
 	end
 	v = unit([kx ky kz]);
     theta = 2*acos(qs);
+    if theta > pi/2
+        theta = pi - theta;
+        v = -v;
+    end
 
     if nargout == 0
         fprintf('Rotation: %f rad x [%f %f %f]\n', theta, v(1), v(2), v(3));
