03b1b7720465816066728be5c1a78f0e1798d31d
pic <pic@gmail.com>
2012-04-28 06:11:15 +0000

    Fixed longstanding problem where plotblock in simulink is very slow unless mouse is over it. - eliminated auto raise and also set delay to zero. - split SerialLink.plot into plot and animate methods, the latter is very low overhead and is used in slplotbot.
    r781 matlab/robot/trunk

--- @SerialLink/animate.m
+++ @SerialLink/animate.m
@@ -1,157 +1,114 @@
-%SerialLink.animate   Update a robot animation
-%
-% R.animate(q) updates an existing animation for the robot R.  This will have
-% been created using R.plot(). Updates graphical instances of this robot in all figures.
-%
-% Notes::
-% - Called by plot() and plot3d() to actually move the arm models.
-% - Used for Simulink robot animation.
-%
-% See also SerialLink.plot.
-
-% Copyright (C) 1993-2015, by Peter I. Corke
-%
-% This file is part of The Robotics Toolbox for MATLAB (RTB).
-% 
-% RTB is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% RTB is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Lesser General Public License for more details.
-% 
-% You should have received a copy of the GNU Leser General Public License
-% along with RTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-% http://www.petercorke.com
-
-function animate(robot, qq)
+function animate(robot, q, handles)
 
     if nargin < 3
         handles = findobj('Tag', robot.name);
     end
-    
-    links = robot.links;
-    N = robot.n;
-    
-    % get handle of any existing graphical robots of same name
-    %  one may have just been created above
-    handles = findobj('Tag', robot.name);
-    
-    % MAIN DISPLAY/ANIMATION LOOP
-    while true
-        % animate over all instances of this robot in different axes
-        
-        for q=qq'  % for all configurations in trajectory
-            q = q';
-            for handle=handles'
-                h = get(handle, 'UserData');
-                
-                % now draw it for a pose q
-                if robot.mdh
-                    % modified DH case
-                    T = robot.base;
-                    vert = transl(T)';
-                    
-                    for L=1:N
-                        if robot.links(L).isprismatic()
-                            % scale the box representing the prismatic joint
-                            % it is based at the origin and extends in z-direction                            
-                            if q(L) > 0
-                                set(h.pjoint(L), 'Matrix', diag([1 1 q(L) 1]));
-                            else
-                                % if length is zero the matrix is singular and MATLAB complains
-                                %error('Prismatic length must be > 0');
-                            end
-                        end
-                        T = T * links(L).A(q(L));
-                        set(h.link(L), 'Matrix', T); 
-                        vert = [vert; transl(T)'];
-                    end
-                    % update the transform for link N+1 (the tool)
-                    T = T * robot.tool;
-                    if length(h.link) > N
-                        set(h.link(N+1), 'Matrix', T);
-                    end
-                    vert = [vert; transl(T)'];
-                else
-                    % standard DH case
-                    T = robot.base;
-                    vert = transl(T)';
-                    
-                    for L=1:N
-                        % for all N links
-                        
-                        if robot.links(L).isprismatic()
-                            % scale the box representing the prismatic joint
-                            % it is based at the origin and extends in z-direction
-                            if q(L) > 0
-                                set(h.pjoint(L), 'Matrix', diag([1 1 q(L) 1]));
-                            else
-                                % if length is zero the matrix is singular and MATLAB complains
-                                %error('Prismatic length must be > 0');
-                            end
-                        end
-                        
-                        % now set the transform for frame {L}, this controls the displayed pose of:
-                        %   the pipes associated with link L, that join {L} back to {L-1}
-                        %   (optional) a prismatic joint L, that joins {L} to {L+1}
-                        if h.link(L) ~= 0
-                            % for plot3d, skip any 0 in the handle list
-                            set(h.link(L), 'Matrix', T);
-                        end
-                        
-                        T = T * links(L).A(q(L));
-                        vert = [vert; transl(T)'];
-                    end
-                    % update the transform for link N+1 (the tool)
-                    T = T*robot.tool;
-                    if length(h.link) > N
-                        set(h.link(N+1), 'Matrix', T);
-                    end
 
-                    vert = [vert; transl(T)'];
-                end
-                
-                % now draw the shadow
-                if isfield(h, 'shadow')
-                    set(h.shadow, 'Xdata', vert(:,1), 'Ydata', vert(:,2), ...
-                        'Zdata', h.floorlevel*ones(size(vert(:,1))));
-                end
-                
-                % update the tool tip trail
-                if isfield(h, 'trail')
-                    T = robot.fkine(q);
-                    robot.trail = [robot.trail; transl(T)'];
-                    set(h.trail, 'Xdata', robot.trail(:,1), 'Ydata', robot.trail(:,2), 'Zdata', robot.trail(:,3));
-                end
-                
-                % animate the wrist frame
-                if ~isempty(h.wrist)
-                    trplot(h.wrist, T);
-                end
-                
-                % add a frame to the movie
-                if ~isempty(h.robot.framenum)
-                    % write the frame to the movie folder
-                    print( '-dpng', fullfile(h.robot.moviepath, sprintf('%04d.png', h.robot.framenum)) );
-                    h.robot.framenum = h.robot.framenum+1;
-                end
-                
-                if h.robot.delay > 0
-                    pause(h.robot.delay);
-                    drawnow
-                end
-                h.q = q;
-                set(handle, 'UserData', h);
-            end
-        end
-        
-        if ~h.robot.loop
-            break;
-        end        
+    for handle=handles'     % for each graphical robot instance
+        h = get(handle, 'UserData');
+        animate2( h, q);
+    end
+
+
+%ANIMATE   move an existing graphical robot
+%
+%   animate(robot, q)
+%
+% Move the graphical robot to the pose specified by the joint coordinates q.
+% Graphics are defined by the handle structure robot.handle.
+
+function animate2(h, q)
+
+    robot = h.robot;
+    n = robot.n;
+    L = robot.links;
+
+    mag = h.mag;
+
+    % compute the link transforms, and record the origin of each frame
+    % for the animation.
+    t = robot.base;
+    Tn = t;
+    x = zeros(1,n);
+    y = zeros(1,n);
+    z = zeros(1,n);
+    xs = zeros(1,n);
+    ys = zeros(1,n);
+    zs = zeros(1,n);
+
+    for j=1:n
+        Tn(:,:,j) = t;
+
+        t = t * L(j).A(q(j));
+
+        x(j) = t(1,4);
+        y(j) = t(2,4);
+        z(j) = t(3,4);
+        xs(j) = t(1,4);
+        ys(j) = t(2,4);
+        zs(j) = h.zmin;
+    end
+    t = t *robot.tool;
+
+    %
+    % draw the robot stick figure and the shadow
+    %
+    set(h.links,'xdata', x, 'ydata', y, 'zdata', z);
+    if isfield(h, 'shadow')
+        set(h.shadow,'xdata', xs, 'ydata', ys, 'zdata', zs);
     end
     
+
+    %
+    % display the joints as cylinders with rotation axes
+    %
+    if isfield(h, 'joint')
+        xyz_line = [0 0; 0 0; -2*mag 2*mag; 1 1];
+
+        for j=1:n
+            % get coordinate data from the cylinder
+            xyz = get(h.joint(j), 'UserData');
+            xyz = Tn(:,:,j) * xyz;
+            ncols = numcols(xyz)/2;
+            xc = reshape(xyz(1,:), 2, ncols);
+            yc = reshape(xyz(2,:), 2, ncols);
+            zc = reshape(xyz(3,:), 2, ncols);
+
+            set(h.joint(j), 'Xdata', xc, 'Ydata', yc, ...
+                'Zdata', zc);
+
+            xyzl = Tn(:,:,j) * xyz_line;
+            if isfield(h, 'jointaxis')
+                set(h.jointaxis(j), 'Xdata', xyzl(1,:), ...
+                    'Ydata', xyzl(2,:), ...
+                    'Zdata', xyzl(3,:));
+                set(h.jointlabel(j), 'Position', xyzl(1:3,1));
+            end
+        end
+    end
+
+    %
+    % display the wrist axes and labels
+    %
+    if isfield(h, 'x')
+        %
+        % compute the wrist axes, based on final link transformation
+        % plus the tool transformation.
+        %
+        xv = t*[mag;0;0;1];
+        yv = t*[0;mag;0;1];
+        zv = t*[0;0;mag;1];
+
+        %
+        % update the line segments, wrist axis and links
+        %
+        set(h.x,'xdata',[t(1,4) xv(1)], 'ydata', [t(2,4) xv(2)], ...
+            'zdata', [t(3,4) xv(3)]);
+        set(h.y,'xdata',[t(1,4) yv(1)], 'ydata', [t(2,4) yv(2)], ...
+             'zdata', [t(3,4) yv(3)]);
+        set(h.z,'xdata',[t(1,4) zv(1)], 'ydata', [t(2,4) zv(2)], ...
+             'zdata', [t(3,4) zv(3)]);
+        set(h.xt, 'Position', xv(1:3));
+        set(h.yt, 'Position', yv(1:3));
+        set(h.zt, 'Position', zv(1:3));
+    end
